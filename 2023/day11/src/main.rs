7e7e18f0e4b104a12174889abcc5aec67655297f
Marc Emmers <35759328+marcemmers@users.noreply.github.com>
2023-12-11 17:33:39 +0100

    Day11 part2 plus cleanup

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -1,27 +1,28 @@
-use std::collections::HashSet;
-use std::time::Instant;
 use std::fs;
+use std::time::Instant;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone)]
 struct Point {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Point {
-    fn get_distance(&self, other: &Point) -> i32 {
+    fn get_distance(&self, other: &Point) -> i64 {
         (self.x - other.x).abs() + (self.y - other.y).abs()
     }
 }
 
+fn collect_points(input: &Vec<&str>) -> Vec<Point> {
+    let mut result = Vec::new();
 
-fn collect_points(input: &Vec<String>) -> HashSet<Point> {
-    let mut result = HashSet::new();
-
-    for (y, line) in input.iter().rev().enumerate() {
+    for (y, line) in input.iter().enumerate() {
         for (x, ch) in line.chars().enumerate() {
             if ch != '.' {
-                result.insert(Point{x: x as i32, y: y as i32});
+                result.push(Point {
+                    x: x as i64,
+                    y: y as i64,
+                });
             }
         }
     }
@@ -29,74 +30,60 @@ fn collect_points(input: &Vec<String>) -> HashSet<Point> {
     return result;
 }
 
-fn solve1(filename: &str) -> u64 {
+fn solve(filename: &str, empty_space: i64) -> u64 {
     println!("Solving for file: {filename}");
     let input = fs::read_to_string(filename).expect("Should have been read");
 
     let lines: Vec<&str> = input.lines().collect();
 
-    let mut puzzle: Vec<String> = Vec::new();
+    let mut galaxies = collect_points(&lines);
 
-    for line in lines.iter() {
-        if line.contains("#") {
-            puzzle.push(line.to_string());
-        } else {
-            puzzle.push(line.to_string());
-            puzzle.push(line.to_string());
+    let increment = empty_space - 1;
+
+    for (cur_y, line) in lines.iter().enumerate().rev() {
+        if !line.contains("#") {
+            galaxies.iter_mut().for_each(|galaxy| {
+                if galaxy.y > cur_y as i64 {
+                    galaxy.y += increment
+                }
+            });
         }
     }
 
-    let lines_it = lines.iter().map(|x| x.chars().collect::<Vec<char>>());
+    let lines_it: Vec<Vec<char>> = lines.iter().map(|x| x.chars().collect::<Vec<char>>()).collect();
 
     for idx in (0..lines[0].len()).rev() {
-        if lines_it.clone().all(|x| x[idx] == '.') {
-            for line in puzzle.iter_mut() {
-                line.insert(idx, '.');
-            }
+        if lines_it.iter().all(|x| x[idx] == '.') {
+            galaxies.iter_mut().for_each(|galaxy| {
+                if galaxy.x > idx as i64 {
+                    galaxy.x += increment
+                }
+            });
         }
     }
 
-    for line in puzzle.iter() {
-        println!("{line}");
-    }
-
-    let set = collect_points(&puzzle);
-
-    let mut set_it = set.iter();
-    set_it.next();
-    let mut sum = 0;
-
-    set.iter().for_each(|p1| {
-        sum += set_it.clone().fold(0, |acc, p2| acc + p1.get_distance(&p2));
-        set_it.next();
-    });
+    let sum: i64 = galaxies
+        .iter()
+        .scan(galaxies.iter(), |it, p1| {
+            it.next();
+            Some(it.clone().fold(0, |acc, p2| acc + p1.get_distance(&p2)))
+        })
+        .sum();
 
     return sum as u64;
 }
-
-
-fn solve2(filename: &str) -> u64 {
-    println!("Solving for file: {filename}");
-    let input = fs::read_to_string(filename).expect("Should have been read");
-
-    let lines = input.lines();
-
-    return 0;
-}
-
 
 const PUZZLE_FILENAME: &'static str = "./src/puzzle.txt";
 
 fn main() {
     let start = Instant::now();
-    println!("Result of 1: {}", solve1(PUZZLE_FILENAME));
+    println!("Result of 1: {}", solve(PUZZLE_FILENAME, 2));
     println!("Solved 1 in {:?}\n\n", start.elapsed());
 
     let start = Instant::now();
-    println!("Result of 2: {}", solve2(PUZZLE_FILENAME));
+    println!("Result of 2: {}", solve(PUZZLE_FILENAME, 1_000_000));
     println!("Solved 2 in {:?}", start.elapsed());
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -106,11 +93,12 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(solve1(EXAMPLE_FILENAME), 374);
+        assert_eq!(solve(EXAMPLE_FILENAME, 2), 374);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(solve2(EXAMPLE_FILENAME), 0);
+        assert_eq!(solve(EXAMPLE_FILENAME, 10), 1030);
+        assert_eq!(solve(EXAMPLE_FILENAME, 100), 8410);
     }
 }
