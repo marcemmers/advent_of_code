b586bc4b5d6219a8851f72fcc2720895f69cd282
Marc <marc_emmers@hotmail.com>
2024-11-16 17:06:33 +0100

    Prepare for 2024

---
+++ 2024/day01/src/main.rs
@@ -0,0 +1,52 @@
+use std::time::Instant;
+use std::fs;
+
+fn solve1(filename: &str) -> u64 {
+    println!("Solving for file: {filename}");
+    let input = fs::read_to_string(filename).expect("Should have been read");
+
+    let lines = input.lines();
+
+    return 0;
+}
+
+
+fn solve2(filename: &str) -> u64 {
+    println!("Solving for file: {filename}");
+    let input = fs::read_to_string(filename).expect("Should have been read");
+
+    let lines = input.lines();
+
+    return 0;
+}
+
+
+const PUZZLE_FILENAME: &'static str = "./src/puzzle.txt";
+
+fn main() {
+    let start = Instant::now();
+    println!("Result of 1: {}", solve1(PUZZLE_FILENAME));
+    println!("Solved 1 in {:?}\n\n", start.elapsed());
+
+    let start = Instant::now();
+    println!("Result of 2: {}", solve2(PUZZLE_FILENAME));
+    println!("Solved 2 in {:?}", start.elapsed());
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_FILENAME: &'static str = "./src/example.txt";
+
+    #[test]
+    fn test1() {
+        assert_eq!(solve1(EXAMPLE_FILENAME), 0);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(solve2(EXAMPLE_FILENAME), 0);
+    }
+}
