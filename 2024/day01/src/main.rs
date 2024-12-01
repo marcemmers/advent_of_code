5c20687da7b9481415378aaa7b740f7e686f24c1
Marc <marc_emmers@hotmail.com>
2024-12-01 10:42:48 +0100

    2024 Day 1

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,13 +1,29 @@
 use std::fs;
 use std::time::Instant;
 
+fn parse_line(line: &str) -> (u64, u64) {
+    let line: Vec<u64> = line
+        .split_whitespace()
+        .map(|val| val.parse::<u64>().expect("Should be able to parse"))
+        .collect();
+    (line[0], line[1])
+}
+
 fn solve1(filename: &str) -> u64 {
     println!("Solving for file: {filename}");
     let input = fs::read_to_string(filename).expect("Should have been read");
 
     let lines = input.lines();
+    let (mut list_a, mut list_b): (Vec<u64>, Vec<u64>) = lines.map(parse_line).collect();
 
-    0
+    list_a.sort();
+    list_b.sort();
+
+    list_a
+        .iter()
+        .zip(list_b.iter())
+        .map(|(a, b)| a.max(b) - a.min(b))
+        .sum()
 }
 
 fn solve2(filename: &str) -> u64 {
@@ -15,8 +31,12 @@ fn solve2(filename: &str) -> u64 {
     let input = fs::read_to_string(filename).expect("Should have been read");
 
     let lines = input.lines();
+    let (list_a, list_b): (Vec<u64>, Vec<u64>) = lines.map(parse_line).collect();
 
-    0
+    list_a
+        .iter()
+        .map(|a| list_b.iter().filter(|b| a == *b).count() as u64 * a)
+        .sum()
 }
 
 const PUZZLE_FILENAME: &str = "./src/puzzle.txt";
@@ -39,11 +59,11 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(solve1(EXAMPLE_FILENAME), 0);
+        assert_eq!(solve1(EXAMPLE_FILENAME), 11);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(solve2(EXAMPLE_FILENAME), 0);
+        assert_eq!(solve2(EXAMPLE_FILENAME), 31);
     }
 }
