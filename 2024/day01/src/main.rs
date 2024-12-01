b49d4eac66efaa961173fea81437cf0dae4197f0
Marc <marc_emmers@hotmail.com>
2024-12-01 11:03:15 +0100

    Optimize 2024 day1

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::collections::HashMap;
 use std::time::Instant;
 
 fn parse_line(line: &str) -> (u64, u64) {
@@ -9,10 +9,7 @@ fn parse_line(line: &str) -> (u64, u64) {
     (line[0], line[1])
 }
 
-fn solve1(filename: &str) -> u64 {
-    println!("Solving for file: {filename}");
-    let input = fs::read_to_string(filename).expect("Should have been read");
-
+fn solve1(input: &str) -> u64 {
     let lines = input.lines();
     let (mut list_a, mut list_b): (Vec<u64>, Vec<u64>) = lines.map(parse_line).collect();
 
@@ -26,28 +23,24 @@ fn solve1(filename: &str) -> u64 {
         .sum()
 }
 
-fn solve2(filename: &str) -> u64 {
-    println!("Solving for file: {filename}");
-    let input = fs::read_to_string(filename).expect("Should have been read");
-
+fn solve2(input: &str) -> u64 {
     let lines = input.lines();
     let (list_a, list_b): (Vec<u64>, Vec<u64>) = lines.map(parse_line).collect();
 
-    list_a
-        .iter()
-        .map(|a| list_b.iter().filter(|b| a == *b).count() as u64 * a)
-        .sum()
+    let mut map: HashMap<u64, u64> = HashMap::new();
+    list_b.iter().for_each(|b| *map.entry(*b).or_insert(0) += 1);
+    list_a.iter().map(|a| map.get(a).unwrap_or(&0) * a).sum()
 }
 
-const PUZZLE_FILENAME: &str = "./src/puzzle.txt";
+const PUZZLE: &str = include_str!("./puzzle.txt");
 
 fn main() {
     let start = Instant::now();
-    println!("Result of 1: {}", solve1(PUZZLE_FILENAME));
+    println!("Result of 1: {}", solve1(PUZZLE));
     println!("Solved 1 in {:?}\n\n", start.elapsed());
 
     let start = Instant::now();
-    println!("Result of 2: {}", solve2(PUZZLE_FILENAME));
+    println!("Result of 2: {}", solve2(PUZZLE));
     println!("Solved 2 in {:?}", start.elapsed());
 }
 
@@ -55,15 +48,15 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE_FILENAME: &str = "./src/example.txt";
+    const EXAMPLE: &str = include_str!("./example.txt");
 
     #[test]
     fn test1() {
-        assert_eq!(solve1(EXAMPLE_FILENAME), 11);
+        assert_eq!(solve1(EXAMPLE), 11);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(solve2(EXAMPLE_FILENAME), 31);
+        assert_eq!(solve2(EXAMPLE), 31);
     }
 }
