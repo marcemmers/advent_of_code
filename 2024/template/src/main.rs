025320d24dffda0af6afccb69a805ac0170d6ed7
Marc <marc_emmers@hotmail.com>
2024-12-01 11:03:50 +0100

    Embed puzzles into binary instead of reading from OS

--- 2024/template/src/main.rs
+++ 2024/template/src/main.rs
@@ -1,33 +1,26 @@
-use std::fs;
 use std::time::Instant;
 
-fn solve1(filename: &str) -> u64 {
-    println!("Solving for file: {filename}");
-    let input = fs::read_to_string(filename).expect("Should have been read");
-
+fn solve1(input: &str) -> u64 {
     let lines = input.lines();
 
     0
 }
 
-fn solve2(filename: &str) -> u64 {
-    println!("Solving for file: {filename}");
-    let input = fs::read_to_string(filename).expect("Should have been read");
-
+fn solve2(input: &str) -> u64 {
     let lines = input.lines();
 
     0
 }
 
-const PUZZLE_FILENAME: &str = "./src/puzzle.txt";
+const PUZZLE: &str = include_str!("./puzzle.txt");
 
 fn main() {
     let start = Instant::now();
-    println!("Result of 1: {}", solve1(PUZZLE_FILENAME));
+    println!("Result of 1: {}", solve1(PUZZLE));
     println!("Solved 1 in {:?}\n\n", start.elapsed());
 
     let start = Instant::now();
-    println!("Result of 2: {}", solve2(PUZZLE_FILENAME));
+    println!("Result of 2: {}", solve2(PUZZLE));
     println!("Solved 2 in {:?}", start.elapsed());
 }
 
@@ -35,15 +28,15 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE_FILENAME: &str = "./src/example.txt";
+    const EXAMPLE: &str = include_str!("./example.txt");
 
     #[test]
     fn test1() {
-        assert_eq!(solve1(EXAMPLE_FILENAME), 0);
+        assert_eq!(solve1(EXAMPLE), 0);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(solve2(EXAMPLE_FILENAME), 0);
+        assert_eq!(solve2(EXAMPLE), 0);
     }
 }
